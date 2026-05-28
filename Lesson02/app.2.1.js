d2f8101657d43d476596dc0a7aad275bfa88c833
Nepochtov Dmitry <dimendroider@gmail.com>
2026-05-29 00:01:15 +0300

    Lesson02 practice

---
+++ Lesson02/app.2.1.js
@@ -0,0 +1,17 @@
+/** 
+ * Каковы типы переменных, перечисленных ниже? Проверяйте с помощью
+оператора typeof и выводите результаты на экран.
+ */
+
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1024;
+
+console.log('str1: ', str1, typeof str1);
+console.log('str2: ', str2, typeof str2);
+console.log('val1: ', val1, typeof val1);
+console.log('val2: ', val2, typeof val2);
+console.log('myNum: ', myNum, typeof myNum);
+
