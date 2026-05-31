69e27bd847e9b9d3a548a046ffae76d04bdfa0f2
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2026-05-31 20:37:23 +0300

    practice 1.3 add two .js files

--- Lesson01/app.js
+++ Lesson01/app.js
@@ -1,2 +1,3 @@
 console.log('Hi there! we use JS in different separeted file');
+console.log('Hi! WITH defer in HEAD');
 alert('Hi there');
---
+++ Lesson01/smth.js
@@ -0,0 +1,21 @@
+/**
+ * test js formating code
+ */
+console.log('Hi! without defer in bottom');
+
+
+const state = 'new';
+let scared = false;
+
+if (state === 'new') {
+  console.log('Welcome to JS');
+  
+  if (scared) {
+    console.log('dont be scared! Dont worry be happy, you will be fine');
+  } else {
+    console.log('you`re brave! you are going to do great');
+  }
+} else {
+  
+  console.log('Welcome back, old school');
+}
