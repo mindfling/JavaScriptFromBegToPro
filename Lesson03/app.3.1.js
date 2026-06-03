8109a1a8b5af53c66c3d650152bd51eb7293abfd
Nepochtov Dmitry <dimendroider@gmail.com>
2026-06-04 01:36:06 +0300

    practice 3.1

---
+++ Lesson03/app.3.1.js
@@ -0,0 +1,22 @@
+// * Practice exercise 3.1
+
+const shoppingList = [
+  'Milk',
+  'Bread',
+  'Apples',
+];
+
+console.log('shoppingList: ', shoppingList);
+console.log('length: ', shoppingList.length);
+
+shoppingList[1] = 'Bananas';
+
+console.log('after shoppingList', shoppingList);
+
+
+/**
+ * 1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and "Apples."
+ * 2. Check your list length in the console.
+ * 3. Update "Bread" to "Bananas."
+ * 4. Output your entire list to the console.
+ */
