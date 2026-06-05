b50154677da8921283dba744b18c2b418f7e6d28
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2026-06-05 20:38:35 +0300

    practice 2.2

---
+++ Lesson02/app.2.2.js
@@ -0,0 +1,13 @@
+const name = 'DIm';
+const age = 21;
+const canCode = true;
+
+console.log('name: ', name);
+console.log('age: ', age);
+console.log('canCode: ', canCode, !!canCode);
+
+if (canCode) {
+  console.log('Yes, I realy cool can write code');
+} else {
+  console.log('Sorry, you cant coding');
+}
