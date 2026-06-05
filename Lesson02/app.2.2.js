674128ceffc1eff75e64c40f2b1f868cc7c0720e
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2026-06-05 20:47:23 +0300

    practice 2.2 canCode

--- Lesson02/app.2.2.js
+++ Lesson02/app.2.2.js
@@ -1,10 +1,14 @@
 const name = 'DIm';
 const age = 21;
-const canCode = true;
+const canCode = prompt('Tell me if you can code')
+// const canCode = true;
 
 console.log('name: ', name);
 console.log('age: ', age);
 console.log('canCode: ', canCode, !!canCode);
+
+console.log(`Hello! My name is ${name} and I\'m ${age} years old, and I canCode: ${!!canCode}`);
+console.log("Hello! My name is", name, "and I\'m", age, "years old, and I canCode:", !!canCode);
 
 if (canCode) {
   console.log('Yes, I realy cool can write code');
