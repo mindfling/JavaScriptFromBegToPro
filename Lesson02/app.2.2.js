56e2d7fd954a6929c0af90220fd7b32437fb7f75
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2026-06-06 12:49:22 +0300

    practice 2.2 check if u can code

--- Lesson02/app.2.2.js
+++ Lesson02/app.2.2.js
@@ -1,17 +1,21 @@
+console.log('exersice 2.2');
+
 const name = 'DIm';
 const age = 21;
-const canCode = prompt('Tell me if you can code')
-// const canCode = true;
+const canCode = true;
+// const canCode = prompt('Tell me if you can code')
 
-console.log('name: ', name);
-console.log('age: ', age);
-console.log('canCode: ', canCode, !!canCode);
+// console.log('name: ', name);
+// console.log('age: ', age);
+// console.log('canCode: ', canCode, !!canCode);
 
-console.log(`Hello! My name is ${name} and I\'m ${age} years old, and I canCode: ${!!canCode}`);
-console.log("Hello! My name is", name, "and I\'m", age, "years old, and I canCode:", !!canCode);
+console.log(`Hello! My name is ${name} and I'm ${age} years old, and I canCode: ${!!canCode}`);
+console.log('Hello! My name is', name, 'and I\'m', age, "years old, and I canCode:", !!canCode);
+console.log("Hello! My name is " + name + " and I'm " + age + " years old, and I canCode: " + !!canCode);
 
 if (canCode) {
   console.log('Yes, I realy cool can write code');
 } else {
   console.log('Sorry, you cant coding');
 }
+
